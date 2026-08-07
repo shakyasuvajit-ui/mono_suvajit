631611dc8c19ab212339eddcac6ac380ff1ec63a
Suvajit Shakya <shakyasuvajit@gmail.com>
2026-08-07 07:12:28 +0545

    min update

--- app/(home)/homepage.tsx
+++ app/(home)/homepage.tsx
@@ -1,9 +1,17 @@
 import React from 'react';
 import { View, Text } from 'react-native';
+import  PageBackground  from "@/assets/svg/page_bg.svg";
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+  container:{
+    flex:1
+  }
+}); 
 
 export default function Homepage() {
   return (
-    <View>
+    <View style={styles.container}>
       <Text>Homepage</Text>
     </View>
   );
